2868b2c0b245dcc99c595c995d3409475ba36e86
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-12-04 15:09:06 +0800

    empty to copy & paste

---
+++ 2024/src/days/empty.rs
@@ -0,0 +1,32 @@
+use colored::Colorize;
+
+#[allow(unused)]
+const INPUT: &str = include_str!("../../input/4.txt");
+#[allow(unused)]
+const EXAMPLE_INPUT_1: &str = "";
+#[allow(unused)]
+const EXAMPLE_INPUT_2: &str = "";
+pub fn go()
+{
+	println!("Day 4");
+
+	println!("\t{}\n\tAAA: {}",
+		"Part 1".bold(),
+		part_one()
+	);
+	
+	println!("\t{}\n\tAAA: {}",
+		"Part 2".bold(),
+		part_two()
+	);
+}
+
+fn part_one() -> i32
+{
+	0
+}
+
+fn part_two() -> i32
+{
+	0
+}
