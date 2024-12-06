5294317696b44c99ffadb6545399e1d786d60de7
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-12-06 12:42:21 +0800

    fancy new new_day script

---
+++ 2024/new_day.sh
@@ -0,0 +1,24 @@
+
+day_text=("one" "two" "three" "four" "five" "six" "seven" "eight" "nine" "ten" "eleven" "twelve" "thirteen" "fourteen" "fifteen" "sixteen" "seventeen" "eighteen" "nineteen" "twenty" "twenty_one" "twenty_two" "twenty_three" "twenty_four" "twenty_five")
+
+
+printf "Please enter the new day's num\n[1-25] "
+read day_num
+
+rs_file="src/days/${day_text[$day_num-1]}.rs"
+txt_file="input/$day_num.txt"
+
+printf "\n\t$rs_file\n"
+printf "\t$txt_file\n\n"
+
+if [[ -e "$rs_file" ]]; then
+	printf "That day already exists!\n"
+else
+	printf "That day does not exist!\n"
+fi
+
+cp src/days/empty.rs $rs_file
+sed -i "s/~DAY_NUM~/${day_num-1}/g" $rs_file
+touch $txt_file
+
+printf "\npub mod ${day_text[$day_num-1]};" >> src/days/mod.rs
--- 2024/src/days/empty.rs
+++ 2024/src/days/empty.rs
@@ -1,14 +1,14 @@
 use colored::Colorize;
 
 #[allow(unused)]
-const INPUT: &str = include_str!("../../input/4.txt");
+const INPUT: &str = include_str!("../../input/~DAY_NUM~.txt");
 #[allow(unused)]
 const EXAMPLE_INPUT_1: &str = "";
 #[allow(unused)]
 const EXAMPLE_INPUT_2: &str = "";
 pub fn go()
 {
-	println!("Day 4");
+	println!("Day ~DAY_NUM~");
 
 	println!("\t{}\n\tAAA: {}",
 		"Part 1".bold(),
